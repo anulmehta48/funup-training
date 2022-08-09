6ee11e74d00de99b48148e11cf27f49431cf9c02
anulmehta48 <anulmehta1848@gmail.com>
2022-08-09 07:31:02 +0530

    mid day assignment complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { result } = require('lodash');
 const router = express.Router();
+const lodash=require('lodash')
+
+
 const abc = require('../introduction/intro')
 const xyz = require('../logger/logger')
 const date=require('../util/helper')
@@ -27,8 +31,29 @@ router.get('/test-me', function (req, res) {
     lower.toLowerCase()
     upper.toUpperCase()
 
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    const result=lodash.chunk(monthNames,3)
+    console.log('my chunks group together',result)
+
+
+    const oddNumber=[3,5,7,9,11,13,15,17,19,21]
+    const result2=lodash.tail(oddNumber)
+    console.log('my 9 odd numbers',result2)
+
+    const myArr1=[1,2,3,4,5]
+    const myArr2=[8,9,4,5,7]
+    const myArr3=[5,2,3,4,7]
+    const myArr4=[8,2,7,4,5]
+    const result3=lodash.union(myArr1,myArr2,myArr3,myArr4)
+    console.log(result3)
+
+    const myobject=[["horror","The Shining"], ["drama","Titanic"],["thriller"],["Shutter Island"],["fantasy","Pans Labyrinth"]]
+    const result4=lodash.fromPairs(myobject)
+    console.log(result4)
+
     res.send('My second ever api!')
-  
+
 
 });
 
