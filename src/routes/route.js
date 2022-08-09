85c2a3fc62bdac6509252271b7f579057bd5b1f8
anulmehta48 <anulmehta1848@gmail.com>
2022-08-09 21:34:23 +0530

    sdf

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
-const underScore=require('underscore')
+// const underScore=require('underscore')
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -22,33 +22,62 @@ router.get('/movies', function (req, res){
     let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
     res.send(movies)
 })
-
-router.get('/films', function (req, res){
-    const movie=[{
-        "id":1,
-        "name1":"moive1"},{
-        "id":2,
-        "name2":"moive2"},{
-        "id":3,
-        "name3":"moive3"},{
-        "id":4,
-        "name4":"moive4"},{
-        "id":5,
-        "name5":"moive5"}]
-    res.send(movie)
+router.get('/movies/:indexNumber', function (req, res){
+    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    console.log(req.params.indexNumber)
+    let movieIndex=req.params.indexNumber
+    // check index number 
+    if(movieIndex<0 || movieIndex>=movies.length){
+        res.send("The index value is not correct please check it !")
+    }
+    let getMoives=movies[movieIndex]
+    res.send(getMoives)
 })
 
-// router.get('/movies/:indexNumber', function(req, res){
-//     let requestParams = req.params
-//     console.log("This is the request "+ JSON.stringify(requestParams))
-//     let indexNumber = requestParams.indexNumber
-//     let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-//     movies=indexNumber
-//     console.log('Name of the Movie is ', movies)
+router.get('/films/:filmId', function (req, res){
+    const films= [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+    
+    let filmId=req.params.filmId
 
-//     res.send(movies)
-// })
+    for(let i=0;i<films.length;i++){
+        let Index=films[i]
+        if(Index.id==filmId){
+           return res.send(Index)
+        }
+    }
+    res.send("this film id does not match for any moives ")
+})
 
+
+router.get('/films', function (req, res){
+    const movie= [ {
+            "id": 1,
+            "name": "The Shining"
+           }, {
+            "id": 2,
+            "name": "Incendies"
+           }, {
+            "id": 3,
+            "name": "Rang de Basanti"
+           }, {
+            "id": 4,
+            "name": "Finding Nemo"
+           }]
+           
+    res.send(movie)
+})
 router.get('/student-details/:name', function(req, res){
     /*
     params is an attribute inside request that contains 
@@ -68,6 +97,7 @@ router.get('/student-details/:name', function(req, res){
     
     res.send('Dummy response')
 })
+
 
 
 module.exports = router;
