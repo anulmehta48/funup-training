d042a308638b60f6f8512859869c4f51a4607ee1
anulmehta48 <anulmehta1848@gmail.com>
2022-08-11 07:31:22 +0530

    players

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,174 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
+router.get('/students/:name', function (req, res) {
     let studentName = req.params.name
     console.log(studentName)
     res.send(studentName)
 })
 
-router.get("/random" , function(req, res) {
+
+
+router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+
+    let totalOfArry=0
+
+    for(let i=0;i<arr.length;i++){
+        totalOfArry=totalOfArry+arr[i]
+    }
+    let TakeLastNumber=arr.pop()
+    let conseCutiveTotal=(TakeLastNumber*(TakeLastNumber+1)/2)
+    let missingNumber=conseCutiveTotal-totalOfArry
+    ///LOGIC WILL GO HERE 
+    res.send(  { data: missingNumber  }  );
+});
+
+
+
+router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+
+    let lenOfArray=arr.length
+
+    let totalOfArry=0
+    for(let i=0;i<arr.length;i++){
+        totalOfArry=totalOfArry+arr[i]
+    }
+    let takefirstNumber=arr[0]
+    let TakeLastNumber=arr.pop()
+    let conseCutiveTotal=((lenOfArray+1)*(takefirstNumber+TakeLastNumber)/2)
+    let missingNumber=conseCutiveTotal-totalOfArry
+
+    ///LOGIC WILL GO HERE 
+
+    res.send(  { data: missingNumber  }  );
+});
+
+
+
+
+
+
+router.get("/random", function (req, res) {
     res.send("hi there")
 })
 
 
-router.get("/test-api" , function(req, res) {
+router.get("/test-api", function (req, res) {
     res.send("hi FunctionUp")
 })
 
 
-router.get("/test-api-2" , function(req, res) {
+router.get("/test-api-2", function (req, res) {
     res.send("hi FunctionUp. This is another cool API")
 })
 
 
-router.get("/test-api-3" , function(req, res) {
+router.get("/test-api-3", function (req, res) {
     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
 })
 
 
-router.get("/test-api-4" , function(req, res) {
+router.get("/test-api-4", function (req, res) {
     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
 })
 
 
-
-router.get("/test-api-5" , function(req, res) {
+router.get("/test-api-5", function (req, res) {
     res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
 })
 
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
+router.get("/test-api-6", function (req, res) {
+    res.send({ a: 56, b: 45 })
 })
 
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
+router.post("/test-post", function (req, res) {
+    res.send([23, 45, 6])
 })
 
 
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
+router.post("/test-post-2", function (req, res) {
+    res.send({ msg: "hi", status: true })
 })
 
-router.post("/test-post-3", function(req, res) {
+router.post("/test-post-3", function (req, res) {
     // let id = req.body.user
     // let pwd= req.body.password
 
     // console.log( id , pwd)
 
-    console.log( req.body )
+    console.log(req.body)
 
-    res.send(  { msg: "hi" , status: true }  )
+    res.send({ msg: "hi", status: true })
 })
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
+router.post("/test-post-4", function (req, res) {
+    let arr = [12, "functionup"]
+    let ele = req.body.element
     arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
+    res.send({ msg: arr, status: true })
 })
+
+let players =[
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ],
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ],
+        },
+    ]
+
+
+
+    
+
+router.post('/players', function (req, res) {
+
+    //LOGIC WILL COME HERE
+    let objValue = req.body
+    let objName = objValue.name
+    let forCheckReapet = false
+    for (let i = 0; i < players.length; i++) {
+        if (players[i].name == objName) {
+            forCheckReapet = true
+            break;
+        }
+    }
+    if (forCheckReapet) {
+        res.send("player is already exist")
+    }
+    else {
+        players.push(objValue)
+        res.send(players)
+    }
+})
+
+
 
 module.exports = router;
